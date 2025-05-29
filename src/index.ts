145e4f506fcd92da0a84010fee0ed0d3bcdfb182
wswiselife <wswiselife@gmail.com>
2025-05-29 12:46:54 +0800

    try to connent

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,24 @@
+const twoSum = (arr: number[], target: number): number[] | undefined => {
+    if (arr.length < 2) return;
 
-const isAnagram = (str1: string, str2: string): boolean => {
+    let hashMap: Map<number, number> = new Map();
 
-  if (str1.length !== str2.length) return false
-
-  let anagramMap: Map<string, number> = new Map()
-
-  for (let i = 0, length = str1.length; i < length; i++) {
-    if (anagramMap.has(str1[i])) {
-      // 问题一、键值不是使用str[i]
-      // anagramMap.set(str1, anagramMap.get(str1[i]) + 1)
-      // 问题三、直接获取 map 的值，会报错
-      // anagramMap.set(str1[i], anagramMap.get(str1[i]) + 1)
-      let char = str1[i]
-      anagramMap.set(str1[i], (anagramMap.get(char) || 0) + 1)
-    } else {
-      anagramMap.set(str1[i], 1)
+    for (let i = 0; i < arr.length; i++) {
+        let curNum = arr[i];
+        let targetNum = target - curNum;
+        if (hashMap.has(targetNum)) {
+            let mapValue = hashMap.get(targetNum)!;
+            return [i, mapValue];
+        } else {
+            hashMap.set(arr[i], i);
+        }
     }
+};
 
-    // 问题二、这里使用了str2[1]，而不是str2[i]
-    // if (anagramMap.has(str2[1])) {
-    if (anagramMap.has(str2[i])) {
-      // anagramMap.set(str2[i], anagramMap.get(str2[i]) - 1)
-      let char = str2[i]
-      anagramMap.set(str2[i], (anagramMap.get(char) || 0) - 1)
-    } else {
-      anagramMap.set(str2[i], -1)
-    }
+let arr1 = [4, 9, 6, 2, 7, 8, 1];
+let target1 = 20;
+// let target1 = 5;
 
-  }
+let result = twoSum(arr1, target1);
 
-  let anagramArr = Array.from(anagramMap.values())
-
-  return anagramArr.every((item) => item === 0)
-}
-
-// let str1 = 'qweadsczx', str2 = 'qazewdscx'
-// let str1 = 'qweadsczxrvf', str2 = 'qazewdscrfvx'
-let str1 = 'qweadsczxrvf', str2 = 'qazewdsclfvx'
-
-let result = isAnagram(str1, str2)
-
-console.log('result >>', result);
+console.log("result :>> ", result);
